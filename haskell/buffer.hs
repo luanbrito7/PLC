018bb58a1c4115a5595fc6d761cdf451c7f77df1
Luan Brito <lab7@cin.ufpe.br>
2019-06-12 21:47:41 +0000

    finish buffer

--- haskell/buffer.hs
+++ haskell/buffer.hs
@@ -3,21 +3,59 @@ import Control.Concurrent
 
 main :: IO ()
 main = do
-    
-    lista <- newTVarIO []
-    put lista 10
-    a <- get lista
-    putStrLn $ show a
+    lista <- atomically (newBuffer)
+    fim <- atomically (newTVar 2)
+    forkIO (incrementador lista 1000 fim)
+    forkIO (decrementador lista 1000 fim)
+    forkIO (waitThreads fim)
+    f <- atomically (readTVar fim)
+    print f
     return ()
 
-get :: TVar [Integer] -> IO Integer
-get mem = do atomically(do (x:xs) <- readTVar mem
-                           writeTVar mem xs
-                           return x
-                        )
+    
+type Buffer t = TVar [t]
 
-put :: TVar [Integer] -> Integer -> IO ()
-put mem value = do atomically(do lista <- readTVar mem
-                                 writeTVar mem $ lista ++ [value]
-                                 return ()
-                             )
+incrementador :: Buffer Integer -> Integer -> TVar Integer -> IO ()
+incrementador buffer 0 fim = atomically (do{
+                                                f <- readTVar fim;
+                                                writeTVar fim (f-1);
+                                           }
+                                        )
+incrementador buffer num fim = do atomically(do{ put buffer num})
+                                  incrementador buffer (num-1) fim
+
+decrementador :: Buffer Integer -> Integer -> TVar Integer -> IO ()
+decrementador buffer 0 fim = atomically (do {
+                                                f <- readTVar fim;
+                                                writeTVar fim (f-1);
+                                            }
+                                        )
+decrementador buffer num fim = do atomically(do{  valor <- get buffer;
+                                                  return ()
+                                               }
+                                            )   
+                                  decrementador buffer (num-1) fim
+
+waitThreads :: TVar Integer -> IO ()
+waitThreads fim = atomically(do {
+                                    v <- readTVar fim;
+                                    if v > 0
+                                        then retry
+                                        else return ()
+                                }
+                            )
+    
+newBuffer :: STM (Buffer t)
+newBuffer = newTVar []
+
+get :: Buffer t -> STM (t)
+get mem = do valor <- readTVar mem
+             if null valor
+                then retry
+             else do 
+                writeTVar mem $ tail valor
+                return $ head valor
+
+put :: Buffer t -> t -> STM ()
+put mem value = do  lista <- readTVar mem
+                    writeTVar mem $ lista ++ [value]
